25ea9443c42ed0e7422ac6585489be8e2c87d2be
MARIO EDUARDO PEDRO LIBARDO <0040482521034@americana.fatec.br>
2025-11-26 10:48:01 -0300

    adicionando novas paginas

--- api.js
+++ api.js
@@ -1,15 +1,15 @@
 // Buscando a API:
 
-async function chamarApi() {
-    
+    async function chamarApi() {
+
     try {
         const URL = await fetch('https://dummyjson.com/products');
-        
+
         //fetch() → função usada para buscar dados de um servidor/API.
         //'https://dummyjson.com/products' → URL da API.
         //await → espera a resposta chegar(assíncrona).
         //URL → guarda o retorno da API(não é o JSON ainda).
-        
+
         // Mostra o status da API
         console.log(URL);
 
@@ -19,9 +19,30 @@ async function chamarApi() {
 
             // O resltado da API vai converter para um objeto em json
             const obj = await URL.json();
-
+            const lista = obj.products;
             // Mostra o objeto que esta armazenado em json dentro de obj
             console.log(obj);
+
+            const container = document.getElementById('produtos');
+            container.innerHTML = '';
+
+
+            lista.forEach(prod => {
+                const card = document.createElement('div');
+                card.className = 'card';
+
+
+                card.innerHTML = `
+                    <img src="${prod.thumbnail}" alt="${prod.title}">
+                    <h2>${prod.title}</h2>
+                    <p><strong>Categoria:</strong> ${prod.category}</p>
+                    <p><strong>Marca:</strong> ${prod.brand}</p>
+                    <p class="preco"><strong>R$ </strong>${prod.price}</p>
+                    `;
+
+                container.appendChild(card);
+            }
+            )
         }
 
     } catch (erro) {
@@ -30,5 +51,3 @@ async function chamarApi() {
 }
 
 chamarApi();
-
-
