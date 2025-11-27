3235e01e62fea4d1b7da147a913726ac0e3cd21f
mario-edu <mario.libardo@hotmail.com>
2025-11-27 10:00:26 -0300

    make: adicionado uma biblioteca

---
+++ lenis.js
@@ -0,0 +1,13 @@
+
+// Initialize Lenis
+const lenis = new Lenis({
+    duration: 2
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
